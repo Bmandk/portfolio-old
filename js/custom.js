52140efeb73bcceebadf04f60a21c315d1d33ae5
Jonathan Hertz <jonathan_hertz2@live.dk>
2019-02-08 21:25:43 +0100

    Implemented Smadreland Simulator Changed social media

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$.get("/navbar.html", function(data){
+		$("nav").replaceWith(data);
+	});
+
+    // Sætter BootStraps modal-vindues-billede til det vi har trykket på og åben modal-vinduet.
+    $('.pop').on('click', function() {
+		$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+		$('#imagemodal').modal('show');   
+	});
+});
